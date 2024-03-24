73b55eb500a644ff251ab19c9442909941cbd8f5
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-03-25 01:01:13 +0400

    I write an float button effect with Javascript

--- src/script/app.js
+++ src/script/app.js
@@ -1,20 +1,7 @@
-let float_btn = document.getElementById("float-btn");
+let btn = document.querySelector(".float-button");
 
-const MakeFloatEeffect = () => {
-  document.addEventListener("scroll", () => {
-    if (Math.floor(window.scrollY) >= 111) {
-      
-        float_btn.style.opacity = 1;
-        float_btn.style.visibility = 'visible';
-        float_btn.style.position = 'absolute';
-        float_btn.style.top = '10px';
-        float_btn.style.right = '10px';
-
-
-      console.log(window.scrollY);
-    } else {
-      float_btn.classList.remove("smooth");
-    }
-  });
-};
-//MakeFloatEeffect();
+btn.addEventListener("click", (e) => {
+  if (e.target) {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+});
