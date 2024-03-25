0dbda6481959eaa6c48bd5eb51c75e014c7c655f
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-03-25 14:40:03 +0400

    I checked and tidied up my codes

--- src/script/app.js
+++ src/script/app.js
@@ -1,5 +1,5 @@
 let btn = document.querySelector(".float-button");
-
+// note:  Float Button Function
 btn.addEventListener("click", (e) => {
   if (e.target) {
     window.scrollTo({ top: 0, behavior: "smooth" });
